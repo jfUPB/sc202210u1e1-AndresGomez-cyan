5d97b3f3b37e73aed55e4a58a3f3bfd941bdd7c9
Andres Gomez <andresmauriciogg@outlook.com>
2022-03-23 06:44:46 -0500

    ?

--- problem/program.asm
+++ problem/program.asm
@@ -1,60 +1,63 @@
 // ANDRES MAURICIO GOMEZ GONZALEZ
 // 000423788
 // andresm.gomezg@upb.co
-(START)
-   @KBO
-   D=MAURICIO@FILLORCLEAR
-   D;JNE
-   @START
-   @;JMP
+
+(START)			// while (true)
+	@KBD
+	D=M
+	@FILLORCLEAR	// if (kbd != 0)
+	D;JNE
+	@START
+	0;JMP
 
 (FILLORCLEAR)
-   @j
-   M = D
-   @70
-   D = D - A
-   @FILL
-   D;JEQ
-   @j
-   D = M
-   @67
-   D = D - A
-   @CLEAR
-   D;JEQ
-   @START
-   0;JMP
+// if key = f --> draw else if key = c --> clear
+	@j
+	M = D // save key
+	@70  // press f
+	D = D-A
+	@FILL
+	D;JEQ
+	@j
+	D = M
+	@67
+	D = D - A
+	@CLEAR
+	D;JEQ
+	@START
+	0;JMP
 
 (FILL)
-   @value
-   M = -1
-   @DRAW
-   0;JMP
+	@value
+	M = -1
+	@DRAW
+	0;JMP
 
 (CLEAR)
-   @value
-   M = 0
-   @DRAW
-   0;JMP
+	@value
+	M = 0
+	@DRAW
+	0;JMP
 
 (DRAW)
-   @SCREEN
-   D = A
-   @i
-   M = D
+	@SCREEN
+	D = A
+	@i
+	M = D
 
 (LOOP)
-   @value
-   D = M
-   @i
-   A = M
-   M = D
-   @i
-   M = M + 1
-   @24576
-   D = A
-   @i
-   D = M - D
-   @LOOP
-   D;JNE
-   @START
-   0;JMP
+	@value
+	D = M
+	@i
+	A = M
+	M = D
+	@i
+	M = M + 1
+	@24576
+	D = A
+	@i
+	D = M - D
+	@LOOP
+	D;JNE
+	@START
+	0;JMP
