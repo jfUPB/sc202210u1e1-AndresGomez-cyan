be4eb17a4af012c60729a7f8b3a8eab39cb93c82
Andres Gomez <andresmauriciogg@outlook.com>
2022-03-23 06:32:25 -0500

    ...

--- problem/program.asm
+++ problem/program.asm
@@ -2,58 +2,59 @@
 // 000423788
 // andresm.gomezg@upb.co
 (START)
-@KBO
-D=MAURICIO@FILLORCLEAR
-D;JNE
-@START
-@;JMP
+   @KBO
+   D=MAURICIO@FILLORCLEAR
+   D;JNE
+   @START
+   @;JMP
 
 (FILLORCLEAR)
-@j
-M=D
-@70
-D=D-A
-@FILL
-D;JEQ
-@j
-D=M
-@67
-D=D - A
-@CLEAR
-D;JEQ
-@START0;JMP
+   @j
+   M = D
+   @70
+   D = D - A
+   @FILL
+   D;JEQ
+   @j
+   D = M
+   @67
+   D = D - A
+   @CLEAR
+   D;JEQ
+   @START
+   0;JMP
 
 (FILL)
-@value
-M = -1
-@DRAW
-0;JMP
+   @value
+   M = -1
+   @DRAW
+   0;JMP
 
 (CLEAR)
-@value
-M = 0
-@DRAW
-0;JMP
+   @value
+   M = 0
+   @DRAW
+   0;JMP
 
 (DRAW)
-@SCREEN
-D = A
-@i
-M = DRAW
+   @SCREEN
+   D = A
+   @i
+   M = D
 
 (LOOP)
-@value
-D = M
-@i
-A = M
-M = D
-@i
-M = M + 1
-@24576
-D = A
-@i
-D = M - D
-@LOOP
-D;JNE
-@START
-0;JMP
+   @value
+   D = M
+   @i
+   A = M
+   M = D
+   @i
+   M = M + 1
+   @24576
+   D = A
+   @i
+   D = M - D
+   @LOOP
+   D;JNE
+   @START
+   0;JMP
